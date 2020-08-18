1d339170c9095f9ad673978a4fadb4099d2cfd10
PSantosWave <s.paula@ua.pt>
2020-08-18 07:30:52 +0100

    prepared visual for members page

--- src/components/header.php
+++ src/components/header.php
@@ -12,17 +12,17 @@ echo <<<_INIT
     <meta charset='utf-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200&display=swap" rel="stylesheet">
-    <link rel='stylesheet' href='/styles/jquery.mobile-1.4.5.min.css'>
-    <link rel='stylesheet' href='/styles/mainStyles.css' type='text/css'>
-    <link rel='stylesheet' href='/styles/componentsStyles.css' type='text/css'>
-    <script src='/javascript/javascript.js'></script>
-    <script src='/javascript/jquery-2.2.4.min.js'></script>
-    <script src='/javascript/jquery.mobile-1.4.5.min.js'></script>
+    <link rel='stylesheet' href='styles/jquery.mobile-1.4.5.min.css'>
+    <link rel='stylesheet' href='styles/mainStyles.css' type='text/css'>
+    <link rel='stylesheet' href='styles/componentsStyles.css' type='text/css'>
+    <script src='javascript/javascript.js'></script>
+    <script src='javascript/jquery-2.2.4.min.js'></script>
+    <script src='javascript/jquery.mobile-1.4.5.min.js'></script>
 
 _INIT;
 
 require_once 'functions.php';
-
+$currentPath = $_SERVER['REQUEST_URI'];
 $userstr = '';
 
 if (isset($_SESSION['user'])) {
@@ -47,47 +47,46 @@ echo <<<_MAIN
 
 _MAIN;
 
+
 if (!$loggedin) {
-  echo <<<_LOGGEDIN
-        <div class='navBar'>
-          <button class='navBarButton' data-inline='true' data-transition='slide' title='Home' onclick="location.href = 'members.php?view=$user';">
+  echo "<div class='navBar'>
+          <button class='navBarButton " . ($currentPath === '/members.php?view=' . $user ? 'navBarButtonActive' : '') . "' data-inline='true' data-transition='slide' title='Home' onclick=\"location.href = 'members.php?view=$user';\">
             <img class='navBarIcon' src='/images/home.svg'/>
             <label class='navBarTitle'>
               Home
             </label>
           </button>
-          <button class='navBarButton' data-inline='true' data-transition='slide' title='Members' onclick="document.location.href='members.php'">
+          <button class='navBarButton " . ($currentPath === '/members.php' ? 'navBarButtonActive' : '') . "' data-inline='true' data-transition='slide' title='Members' onclick=\"document.location.href='members.php'\">
             <img class='navBarIcon' src='/images/members.svg'/>
             <label class='navBarTitle'>
               Members
             </label>
           </button>
-          <button class='navBarButton' data-inline='true' data-transition='slide' title='Friends' onclick="location.href = 'friends.php';">
+          <button class='navBarButton " . ($currentPath === '/friends.php' ? 'navBarButtonActive' : '') . "' data-inline='true' data-transition='slide' title='Friends' onclick=\"location.href = 'friends.php';\">
             <img class='navBarIcon' src='/images/friends.svg'/>
             <label class='navBarTitle'>
               Friends
             </label>
           </button>
-          <button class='navBarButton' data-inline='true' data-transition='slide' title='Messages' onclick="location.href = 'messages.php';">
+          <button class='navBarButton " . ($currentPath === '/messages.php' ? 'navBarButtonActive' : '') . "' data-inline='true' data-transition='slide' title='Messages' onclick=\"location.href = 'messages.php';\">
             <img class='navBarIcon' src='/images/messages.svg'/>
             <label class='navBarTitle'>
               Messages
             </label>
           </button>
-          <button class='navBarButton' data-inline='true' data-transition='slide' title='Profile' onclick="location.href = 'profile.php';">
+          <button class='navBarButton " . ($currentPath === '/profile.php' ? 'navBarButtonActive' : '') . "' data-inline='true' data-transition='slide' title='Profile' onclick=\"location.href = 'profile.php';\">
             <img class='navBarIcon' src='/images/profile.svg'/>
             <label class='navBarTitle'>
               Profile
             </label>
           </button>
-          <button class='navBarButton' data-inline='true' data-transition='slide' title='Log Out' onclick="location.href = 'logout.php';">
+          <button class='navBarButton " . ($currentPath === '/logout.php' ? 'navBarButtonActive' : '') . "' data-inline='true' data-transition='slide' title='Log Out' onclick=\"location.href = 'logout.php';\">
             <img class='navBarIcon' src='/images/logout.svg'/>
             <label class='navBarTitle'>
               Log Out
             </label>
           </button>
-        </div>       
-_LOGGEDIN;
+        </div>";
 } else {
   // Menu Buttons to Login
   echo <<<_GUEST
